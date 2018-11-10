28a2b239ef3a08f6ed1a7e722f2b9c87a62de462
Tom F <thomas.flitterman@ynov.com>
2018-11-10 02:04:27 +0100

    Added menu & bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
+// Components go here
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+// Custom modules go here
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
